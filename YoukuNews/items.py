b9634a4390ef80d6128b337fbbe2230212dc014b
liolok <liolokit@gmail.com>
2018-05-16 17:54:55 +0800

    优化详细信息获取方式
    预编译正则表达式.
    修改频道名称及其链接的匹配方式,

--- YoukuNews/items.py
+++ YoukuNews/items.py
@@ -21,9 +21,9 @@ class YoukuItem(Item):
     statcomment = Field()   # 评论数
 
     # 详细信息, 将由 spider.parse_detail() 解析
-    subtitle = Field()  # 副标题
-    category = Field()  # 分类
-    chan_name = Field() # 频道名称
-    chan_link = Field() # 频道链接
+    subtitle = Field()      # 副标题
+    category = Field()      # 分类
+    channel_name = Field()  # 频道名称
+    channel_link = Field()  # 频道链接
 
     # pass
--- YoukuNews/spiders/spider.py
+++ YoukuNews/spiders/spider.py
@@ -2,7 +2,13 @@
 
 from scrapy import Spider, Request
 from YoukuNews.items import YoukuItem
-from re import search
+from re import compile, search
+
+# 预编译正则表达式, 用于解析视频页面详细信息
+re_subtitle = compile(r'subtitle\\\" title=\\\"(.+?)\\\">')
+re_category = compile(r'irCategory\\\" content=\\\"(.+?)\\\"')
+re_channame = compile(r'module_basic_sub.+?(alt.+?\\n\s+(.+?))\\')
+re_chanlink = compile(r'module_basic_sub.+?(//i\.youku\.com/i/(?:[A-Za-z0-9]+))')
 
 
 class YoukuSpider(Spider):
@@ -25,8 +31,8 @@ class YoukuSpider(Spider):
             item['title'] = v.css('.v-link').xpath('./a/@title').extract_first()
             item['thumb'] = v.css('.v-thumb').xpath('./img/@src').re_first('(//.+)')
             item['time'] = v.css('.v-time::text').extract_first()
-            item['statplay'] = v.css('.ico-statplay+span::text').extract_first()          # 播放图标后面的文本
-            item['statcomment'] = v.css('.ico-statcomment+span::text').extract_first()    # 评论图标后面的文本
+            item['statplay'] = v.css('.ico-statplay+span::text').extract_first()  # 播放图标后面的文本
+            item['statcomment'] = v.css('.ico-statcomment+span::text').extract_first()  # 评论图标后面的文本
             list.append(item)  # 追加视频列表
         for item in list:  # 回调 parse_detail() 对列表中的每个 YoukuItem 的视频页面进行解析
             yield Request(url="https:" + item['url'], meta={'list': item}, callback=self.parse_detail)
@@ -35,8 +41,8 @@ class YoukuSpider(Spider):
     def parse_detail(self, response):
         item = response.meta['list']  # 接收 parse_basic() 传入的 YoukuItem
         source = response.body.decode("utf-8")  # 对源码进行正则匹配
-        item['subtitle'] = search(r'subtitle\\\" title=\\\"(.+?)\\\">', source)[1]
-        item['category'] = search(r'irCategory\\\" content=\\\"(.+?)\\\"', source)[1]
-        item['chan_name'] = search(r'alt=\\\"\\\" />\\n\s+(.+?)\\n', source)[1]
-        item['chan_link'] = search(r'<a href=\\\"(//i\.youku\.com/i/(?:[A-Za-z0-9=]+))', source)[1]
+        item['subtitle'] = re_subtitle.search(source).group(1)
+        item['category'] = re_category.search(source).group(1)
+        item['channel_name'] = re_channame.search(source).group(2)
+        item['channel_link'] = "https:" + re_chanlink.search(source).group(1)
         yield item  # 返回填写好的 YoukuItem
