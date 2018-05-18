dcf3fc699b92d81412bab6bdac173cfb82903db0
liolok <liolokit@gmail.com>
2018-05-18 23:13:11 +0800

    视频信息导入MongoDB数据库
    scrapy crawl youku    # youku为爬虫唯一识别名
    定义ItemPipeline, 将爬取的静态信息导出到MongoDB数据库.
    ItemPiplines文档: https://docs.scrapy.org/en/latest/topics/item-pipeline.html#write-items-to-mongodb
    MongoDB文档: https://docs.mongodb.com/manual/tutorial/install-mongodb-on-windows/#configure-a-windows-service-for-mongodb-community-edition

--- YoukuNews/pipelines.py
+++ YoukuNews/pipelines.py
@@ -3,9 +3,35 @@
 # Define your item pipelines here
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html#write-items-to-mongodb
+# The main point of this example is to show \
+# how to use from_crawler() method and how to clean up the resources properly.
+
+from pymongo import MongoClient
 
 
 class YoukunewsPipeline(object):
+    collection_name = 'VideoInfo'
+
+    def __init__(self, mongo_uri, mongo_db):
+        self.mongo_uri = mongo_uri  # 统一资源标识符
+        self.mongo_db = mongo_db  # 数据库名称
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            mongo_uri=crawler.settings.get('MONGO_URI'),
+            mongo_db=crawler.settings.get('MONGO_DATABASE', 'items')
+        )
+
+    def open_spider(self, spider):
+        self.client = MongoClient(self.mongo_uri)
+        self.db = self.client[self.mongo_db]
+
+    def close_spider(self, spider):
+        self.client.close()
+
     def process_item(self, item, spider):
+        # 对集合(collection)插入一个文档(document)
+        self.db[self.collection_name].insert_one(dict(item))
         return item
--- YoukuNews/settings.py
+++ YoukuNews/settings.py
@@ -64,9 +64,14 @@ ROBOTSTXT_OBEY = False
 
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'YoukuNews.pipelines.YoukunewsPipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'YoukuNews.pipelines.YoukunewsPipeline': 300,
+}
+
+# Configure MongoDB, 数据库配置
+# URI格式文档: https://docs.mongodb.com/manual/reference/connection-string/
+MONGO_URI = "127.0.0.1:27017"   # URI, 用于连接应用与MongoDB实例
+MONGO_DATABASE = "YoukuNews"    # 数据库名称
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html
@@ -88,3 +93,4 @@ ROBOTSTXT_OBEY = False
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
