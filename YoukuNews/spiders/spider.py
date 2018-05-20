7535e9afe08f79251ac5ca607fbc8de5a87c77ea
liolok <liolokit@gmail.com>
2018-05-20 15:58:54 +0800

    传送协议及正则预编译
    传送协议统一定义为https
    正则表达式预编译语句位置改为YoukuSpider类内部, 在parse_detail()中使用self调用.

--- YoukuNews/spiders/spider.py
+++ YoukuNews/spiders/spider.py
@@ -4,17 +4,12 @@ from scrapy import Spider, Request
 from YoukuNews.items import YoukuItem
 from re import compile, search
 
-# 预编译正则表达式
-re_link = compile(r'(//v\.youku\.com/v_show/id_([A-Za-z0-9]+))')  # 视频页面链接, 省略==.html结尾
-re_subtitle = compile(r'subtitle\\\" title=\\\"(.+?)\\\">')
-re_category = compile(r'irCategory\\\" content=\\\"(.+?)\\\"')
-re_channame = compile(r'module_basic_sub.+?alt.+?\\n\s+(.+?)\\')
-re_chanlink = compile(r'module_basic_sub.+?(//i\.youku\.com/i/(?:[A-Za-z0-9]+))')
-
 
 class YoukuSpider(Spider):
     name = 'youku'  # 爬虫唯一识别名
     allowed_domains = ['youku.com']  # 爬取域名范围
+
+    scheme = "https:"  # URL传送协议
 
     # 从以下三个目录页面开始解析
     def start_requests(self):
@@ -28,10 +23,10 @@ class YoukuSpider(Spider):
         for v in response.css('.v'):  # 遍历目录页面
             item = YoukuItem()  # 循环实例化 YoukuItem, 并解析基本信息填入
             link = v.css('.v-link').xpath('./a/@href').re('(//v.youku.com/v_show/id_([A-Za-z0-9]+))')
-            item['vid'] = link[1]             # 链接id字段即为vid
-            item['url'] = "https:" + link[0]  # 补全协议类型https
+            item['vid'] = link[1]                   # 链接id字段
+            item['url'] = self.scheme + link[0]     # 补全协议类型
             item['title'] = v.css('.v-link').xpath('./a/@title').extract_first()
-            item['thumb'] = "https:" + v.css('.v-thumb').xpath('./img/@src').re_first('//.+')
+            item['thumb'] = self.scheme + v.css('.v-thumb').xpath('./img/@src').re_first('//.+')
             item['time'] = v.css('.v-time::text').extract_first()
             item['statplay'] = v.css('.ico-statplay+span::text').extract_first()        # 播放图标后面的文本
             item['statcomment'] = v.css('.ico-statcomment+span::text').extract_first()  # 评论图标后面的文本
@@ -39,12 +34,18 @@ class YoukuSpider(Spider):
         for item in list:  # 回调 parse_detail() 对列表中的每个 YoukuItem 的视频页面进行解析
             yield Request(url=item['url'], meta={'list': item}, callback=self.parse_detail)
 
+    # 预编译正则表达式, 用于解析视频页面详细信息
+    re_subtitle = compile(r'subtitle\\\" title=\\\"(.+?)\\\">')                         # 副标题
+    re_category = compile(r'irCategory\\\" content=\\\"(.+?)\\\"')                      # 分类
+    re_channame = compile(r'module_basic_sub.+?alt.+?\\n\s+(.+?)\\')                    # 频道名称
+    re_chanlink = compile(r'module_basic_sub.+?(//i\.youku\.com/i/(?:[A-Za-z0-9]+))')   # 频道链接
+
     # 从视频页面解析详细信息
     def parse_detail(self, response):
         item = response.meta['list']  # 接收 parse_basic() 传入的 YoukuItem
         source = response.body.decode("utf-8")  # 对源码进行正则匹配
-        item['subtitle'] = re_subtitle.search(source).group(1)
-        item['category'] = re_category.search(source).group(1)
-        item['channel_name'] = re_channame.search(source).group(1)
-        item['channel_link'] = "https:" + re_chanlink.search(source).group(1)
+        item['subtitle'] = self.re_subtitle.search(source).group(1)
+        item['category'] = self.re_category.search(source).group(1)
+        item['channel_name'] = self.re_channame.search(source).group(1)
+        item['channel_link'] = self.scheme + self.re_chanlink.search(source).group(1)
         yield item  # 返回填写好的 YoukuItem
