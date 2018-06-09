33bf67fd6c632962ec6812b3ff4e132fdf188b9b
liolok <liolokit@gmail.com>
2018-06-10 00:52:01 +0800

    更新测试脚本
    跟进下载路径和爬虫参数

--- 测试.bat
+++ 测试.bat
@@ -1,4 +1,4 @@
-rd /q /s Downloads
+rd /q /s "D:/YoukuNews/Downloads"
 del crawl.log
-scrapy crawl --logfile=crawl.log youku
+scrapy crawl --logfile=crawl.log youku -a category=jrrm -a page_num=1
 pause
