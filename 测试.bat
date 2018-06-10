76d8cb035dbe848ad16a5286ce4672768d8953a3
liolok <liolokit@gmail.com>
2018-06-11 00:13:01 +0800

    更新测试脚本
    Log Level从DEBUG提升至INFO以精简日志文件内容

--- 测试.bat
+++ 测试.bat
@@ -1,4 +1,4 @@
 rd /q /s "D:/YoukuNews/Downloads"
 del crawl.log
-scrapy crawl --logfile=crawl.log youku -a category=jrrm -a page_num=1
+scrapy crawl --logfile=crawl.log -L INFO youku -a catelog=jrrm -a pages=2
 pause
